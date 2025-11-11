0c528f5ba6f6e1f0a69dae01303b09bc9ff34c41
Gabriel Beltran <dottgabriel@proton.me>
2025-11-10 19:52:49 -0600

    Eliminar sys.path.append de src ya que no es necesario

--- main.py
+++ main.py
@@ -6,9 +6,6 @@ from app.constants.texts import APP_NAME, APP_VERSION
 from app.views.main_window import MainWindow
 
 def main():
-    # Agregar el directorio src al path
-    sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
-    
     app = QApplication(sys.argv)
     app.setApplicationName(APP_NAME)
     app.setApplicationVersion(APP_VERSION)
